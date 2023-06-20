428ff163bedf3ba41223ba07a8ec9a4b4803dc0b
raksha <rkpachole9898@gmail.com>
2023-06-20 10:02:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ContactView from "./Components/ContactView/Index";
+
+function App() {
+  return (
+    <div>
+      <ContactView />
+    </div>
+  );
+}
+
+export default App;
